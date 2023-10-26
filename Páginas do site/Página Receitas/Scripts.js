4fa0078d96bb177ef9232343e4179b26af34b77a
Arthur de Araujo Batista <arthur.araujo.yas@gmail.com>
2023-10-26 00:35:34 -0200

    Update

--- Páginas do site/Página Receitas/Scripts.js
+++ Páginas do site/Página Receitas/Scripts.js
@@ -4,3 +4,21 @@ window.addEventListener("scroll", function() {
     let header = document.querySelector('#header')
     header.classList.toggle('Scroll', window.scrollY > 0)
 })
+
+//----------------------- Script do Ver Mais----------------------- //
+
+var button = document.getElementById("spaceRead");
+var ReadMore = document.getElementById("ReadMore");
+
+button.addEventListener("click", function() {
+
+    // var ReadMore = document.getElementById("ReadMore");
+
+    if(ReadMore.style.display === "none") {
+        ReadMore.style.display = "block";
+        button.textContent = "Ver Menos";
+    } else {
+        ReadMore.style.display = "none";
+        button.textContent = "Ver Mais";
+    }
+});
